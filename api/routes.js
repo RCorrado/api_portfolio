41805a35e5cfc04e0e628bd1d14f7bc312981a74
RCorrado <raulc.corrado@gmail.com>
2021-05-25 09:28:02 -0300

    Added get method for root

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,6 @@
 const contactCtrller = require('./v1/contact');
 
 module.exports = app => {
+    app.get('/', (req, res) => res.status(200).send('Welcome to API v1.'));
     app.post('/contact-me', contactCtrller.sendMail);
 };
--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const PORT = process.env.PORT || '4000';
 const app = express();
 const routes = require('./api/routes')(app);
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
